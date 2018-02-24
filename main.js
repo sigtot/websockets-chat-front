e8018af191d52e0d3c52909e942b7f2dc7241f7a
sigtot <sigurdtotland@gmail.com>
2018-02-24 10:59:53 +0100

    Chat works!

---
+++ main.js
@@ -0,0 +1,46 @@
+function message(msg) {
+  $("#chat-box").append(msg);
+}
+
+var socket;
+function connect(){
+  try{
+
+    var host = "ws://159.65.58.116:8080";
+    socket = new WebSocket(host);
+
+    message('<p class="event">Socket Status: '+socket.readyState);
+
+    socket.onopen = function(){
+      message('<p class="event">Socket Status: '+socket.readyState+' (open)');
+    };
+
+    socket.onmessage = function(msg){
+      message('<p class="message">'+msg.data);
+    };
+
+    socket.onclose = function(){
+      message('<p class="event">Socket Status: '+socket.readyState+' (Closed)');
+    }
+
+  } catch(exception){
+    message('<p>Error'+exception);
+  }
+}
+
+$(document).ready(function() {
+  if(!("WebSocket" in window)){
+    $('#chatLog, input, button, #examples').fadeOut("fast");
+    $('<p>Oh no, you need a browser that supports WebSockets. How about <a href="http://www.google.com/chrome">Google Chrome</a>?</p>').appendTo('#container');
+  }else {
+
+    //The user has WebSockets
+
+    connect();
+  }
+});
+
+$("#chat-send-button").click(function(){
+  console.log($("#chat-input").val());
+  socket.send($("#chat-input").val());
+});
